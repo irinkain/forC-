bce7a2e9126837b992150e99a8d401e9001bd8db
irinkain <45570725+irinkain@users.noreply.github.com>
2020-10-19 23:15:54 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, if you want to start a game, please type Y, else N");
+            var result = Console.ReadLine();
+            Weapons weapons = new Weapons();
+            var counter = 10;
+            if (result == "y")
+            {
+                while (counter != 0)
+                {
+                    Console.WriteLine("Please choose a shooter, type 1 or 2");
+                    var player = Console.ReadLine();
+
+                    if (player == "1")
+                    {
+                        Weapons.ShootFromWeapon1(weapons);
+                        counter--;
+                        if(counter == 0)
+                        {
+                            Weapons.Fight("1");
+                        }
+                    }
+                    else if (player == "2")
+                    {
+                        Weapons.ShootFromWeapon2(weapons);
+                        counter--;
+                        if (counter == 0)
+                        {
+                            Weapons.Fight("2");
+                        }
+                    }
+                }
+            }
+            else Console.WriteLine("Bye ..... ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Weapons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW2
+{
+    public class Weapons
+    {
+        //თითოეულ იარაღს აქვს 10 ტყვია.
+        public int Weapon1 { get; set; } = 10;
+        public  int Weapon2 { get; set; } = 10;
+
+        public static void Fight(string player)
+        {
+            Console.WriteLine($"Player {player} winned!!!");
+        }
+        public static int counter = 10;
+        public static void ShootFromWeapon1(Weapons weapons)
+        {
+            counter-- ;
+            weapons.Weapon1 = counter;
+            Console.WriteLine("Shoot from weapon 1 !!!");
+        }
+        public static void ShootFromWeapon2(Weapons weapons)
+        {
+            counter--;
+            weapons.Weapon2 = counter;
+            Console.WriteLine("Shoot from weapon 2 !!!");
+        }
+    }
+}
